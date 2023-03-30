515ce03ea79be35e30fc398ae5d926a71b905cd9
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-30 12:51:07 +0300

    delete according video

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,15 +1,8 @@
 import './App.css';
-import {ducks} from './demo'
-import DuckItem from './DuckItem'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {ducks.map(duck =>(
-             <DuckItem duck={duck} key={duck.name}/>
-          ))}
-        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
