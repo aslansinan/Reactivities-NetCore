ead0fb85cfb8e9612510943442d4f0f557e41833
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-30 16:10:43 +0300

    detail of data with mediatr

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Activities;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseApiController
+{
+    [HttpGet] //api/activities
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await Mediator.Send(new List.Query());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return await Mediator.Send(new Detail.Query { Id = id });
+    }
+}
---
+++ Application/Activities/Detail.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Detail
+{
+    public class Query : IRequest<Activity>
+    {
+        public Guid Id { get; set; }
+    }
+    
+    
+    public class Handler : IRequestHandler<Query, Activity>
+    {
+        private readonly Context _context;
+    
+        public Handler(Context context)
+        {
+            _context = context;
+        }
+        public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await _context.Activities.FindAsync(request.Id);
+        }
+    }
+
+}
