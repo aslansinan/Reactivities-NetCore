50879578308770b72489364ef3ff96a75e359821
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-31 09:58:15 +0300

    Edit activities

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,8 +22,15 @@ public class ActivitiesController : BaseApiController
     }
 
     [HttpPost]
-    public async Task<IActionResult> CreateActivity([FromBody]Activity activity)
+    public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
     {
-        return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+        return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
 }
