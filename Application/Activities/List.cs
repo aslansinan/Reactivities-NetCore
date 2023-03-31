96df74b34ad9d0e0a3913c65c2aca4ae36ac5fb7
SinanAslan <sinanaslan@derinbilgi.com.tr>
2023-03-31 10:48:31 +0300

    using cancelation token while attempting to much time

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities;
@@ -12,13 +13,29 @@ public class List
     public class Handler : IRequestHandler<Query, List<Activity>>
     {
         private readonly Context _context;
+        private readonly ILogger<List> _logger;
 
-        public Handler(Context context)
+        public Handler(Context context, ILogger<List> logger)
         {
             _context = context;
+            _logger = logger;
         }
         public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
         {
+            // Cancelation token faza sürme durumunda yarıda kesme
+            // try
+            // {
+            //     for (int i = 0; i < 10; i++)
+            //     {
+            //         cancellationToken.ThrowIfCancellationRequested();
+            //         await Task.Delay(1000, cancellationToken);
+            //         _logger.LogInformation($"Task {i} has comlated");
+            //     }
+            // }
+            // catch (Exception e)
+            // {
+            //     _logger.LogInformation("task was cancelled");
+            // }
             return await _context.Activities.ToListAsync();
         }
     }
